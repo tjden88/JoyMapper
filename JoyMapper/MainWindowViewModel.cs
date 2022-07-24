6847c6e863f8a5547a367e24d8ebae37c601e007
Денис (Home) <dultsevdv@gmail.com>
2022-07-25 01:03:33 +0500

    Загрузка списка профилей

--- JoyMapper/MainWindowViewModel.cs
+++ JoyMapper/MainWindowViewModel.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using JoyMapper.Models;
+using JoyMapper.Services;
 using SharpDX.DirectInput;
+using WPR.MVVM.Commands;
 using WPR.MVVM.ViewModels;
 
 namespace JoyMapper
 {
     internal class MainWindowViewModel : ViewModel
     {
+
+        private readonly DataManager _DataManager = new();
 
         public MainWindowViewModel()
         {
@@ -40,6 +44,68 @@ namespace JoyMapper
                 })
                 .ToList();
         }
+
+
+        #region Props
+
+        #region Profiles : List<Profile> - Список профилей
+
+        /// <summary>Список профилей</summary>
+        private List<Profile> _Profiles;
+
+        /// <summary>Список профилей</summary>
+        public List<Profile> Profiles
+        {
+            get => _Profiles;
+            set => Set(ref _Profiles, value);
+        }
+
+        #endregion
+
+        #endregion
+
+
+        #region Commands
+        
+        #region Command LoadProfilesCommand - Загрузить профили
+
+        /// <summary>Загрузить профили</summary>
+        private Command _LoadProfilesCommand;
+
+        /// <summary>Загрузить профили</summary>
+        public Command LoadProfilesCommand => _LoadProfilesCommand
+            ??= new Command(OnLoadProfilesCommandExecuted, CanLoadProfilesCommandExecute, "Загрузить профили");
+
+        /// <summary>Проверка возможности выполнения - Загрузить профили</summary>
+        private bool CanLoadProfilesCommandExecute() => true;
+
+        /// <summary>Логика выполнения - Загрузить профили</summary>
+        private void OnLoadProfilesCommandExecuted() => Profiles = _DataManager.Profiles;
+
+        #endregion
+
+
+        #region Command StartProfileCommand - Запустить профиль
+
+        /// <summary>Запустить профиль</summary>
+        private Command _StartProfileCommand;
+
+        /// <summary>Запустить профиль</summary>
+        public Command StartProfileCommand => _StartProfileCommand
+            ??= new Command(OnStartProfileCommandExecuted, CanStartProfileCommandExecute, "Запустить профиль");
+
+        /// <summary>Проверка возможности выполнения - Запустить профиль</summary>
+        private bool CanStartProfileCommandExecute(object p) => p is Profile;
+
+        /// <summary>Логика выполнения - Запустить профиль</summary>
+        private void OnStartProfileCommandExecuted(object p)
+        {
+            var profile = (Profile)p;
+        }
+
+        #endregion
+
+        #endregion
 
     }
 }
