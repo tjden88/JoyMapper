7632e456d9249f888b959928e14c5d11825dab38
Денис (Home) <dultsevdv@gmail.com>
2022-07-26 01:28:09 +0500

    Сервис работы

---
+++ JoyMapper/Models/JoyState.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using SharpDX.DirectInput;
+
+namespace JoyMapper.Models
+{
+    /// <summary>
+    /// Текущий статус отслеживаемого джойстика
+    /// </summary>
+    internal class JoyState
+    {
+        private Joystick _Joystick;
+
+        public record BtnState(int BtnNumber)
+        {
+            public bool IsPressed { get; set; }
+
+            public List<KeyboardKeyBinding> PressKeyBindings { get; set; }
+            public List<KeyboardKeyBinding> ReleaseKeyBindings { get; set; }
+
+        }
+
+
+        /// <summary> Привязанный джойстик </summary>
+        public Joystick Joystick
+        {
+            get => _Joystick;
+            set
+            {
+                _Joystick = value;
+                _Joystick.Acquire();
+            }
+        }
+
+        /// <summary> Статус используемых кнопок </summary>
+        public List<BtnState> BtnStates { get; set; } = new();
+
+
+        /// <summary>
+        /// Получить различия нажатия используемых между прошлым и текущим состоянием джойстика.
+        /// Сохраняет новое состояние
+        /// </summary>
+        /// <returns></returns>
+        public List<BtnState> GetDifferents()
+        {
+            var result = new List<BtnState>();
+            var joy = Joystick;
+            joy.Poll();
+            var state = joy.GetCurrentState().Buttons;
+
+
+            foreach (var btnState in BtnStates)
+            {
+                var newBtnValue = state[btnState.BtnNumber - 1];
+                if (newBtnValue != btnState.IsPressed)
+                {
+                    result.Add(btnState);
+                    btnState.IsPressed = newBtnValue;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary> Синхронизировать состояние кнопок </summary>
+        public void UpdateBtnStatus()
+        {
+            var joy = Joystick;
+            joy.Poll();
+            var state = joy.GetCurrentState().Buttons;
+            foreach (var btnState in BtnStates)
+            {
+                var newBtnValue = state[btnState.BtnNumber - 1];
+                btnState.IsPressed = newBtnValue;
+            }
+        }
+    }
+}
---
+++ JoyMapper/Services/ProfileWorker.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using JoyMapper.Models;
+using SharpDX.DirectInput;
+
+namespace JoyMapper.Services
+{
+    /// <summary>
+    /// Обработка нажатий кнопок контроллера и эмуляция клавиатурных команд
+    /// </summary>
+    internal class ProfileWorker
+    {
+        // задержка опроса джойстика
+        private const int PollingDelay = 50;
+
+        // задержка между командами ввода клавиатуры
+        private const int InputDelay = 20;
+
+
+        private readonly KeyboardSender _Sender = new();
+
+        // Используемые в профиле джойстики
+        private List<JoyState> _UsedInProfileJoystickStates;
+
+        // Текущий профиль
+        private Profile _ActiveProfile;
+
+        // Активен ли трекинг
+        private bool IsActive { get; set; }
+
+        /// <summary> Запустить отслеживание </summary>
+        public void Start(Profile profile)
+        {
+            if (IsActive)
+                Stop();
+
+
+            _ActiveProfile = profile;
+
+            var keyPatterns = App.DataManager.KeyPatterns
+                .Where(p => profile.KeyPatternsIds.Contains(p.Id))
+                .ToArray();
+
+            var usedJoyNames = keyPatterns
+                .Select(p => p.JoyName)
+                .Distinct();
+
+            var usedJoysticks = new DirectInput()
+                .GetDevices(DeviceClass.GameControl, DeviceEnumerationFlags.AttachedOnly)
+                .Where(d => usedJoyNames.Contains(d.InstanceName))
+                .Select(d => new Joystick(new DirectInput(), d.InstanceGuid))
+                .ToList();
+
+
+
+            _UsedInProfileJoystickStates = usedJoysticks
+                .Select(joy => new JoyState
+                {
+                    Joystick = joy,
+                    BtnStates = keyPatterns
+                        .Where(p=>p.JoyName == joy.Information.InstanceName)
+                        .Select(p=> new JoyState.BtnState(p.JoyKey))
+                        .ToList()
+                })
+                .ToList();
+
+
+            foreach (var joyState in _UsedInProfileJoystickStates)
+            {
+                foreach (var btnState in joyState.BtnStates)
+                {
+                    var pattern = keyPatterns.First(p =>
+                        p.JoyName == joyState.Joystick.Information.InstanceName && p.JoyKey == btnState.BtnNumber);
+                    btnState.PressKeyBindings = pattern.PressKeyBindings;
+                    btnState.ReleaseKeyBindings = pattern.ReleaseKeyBindings;
+                }
+
+                joyState.UpdateBtnStatus();
+            }
+
+            Task.Run(Work);
+
+        }
+
+        /// <summary> Остановить отслеживание </summary>
+        public void Stop()
+        {
+            IsActive = false;
+            Thread.Sleep(PollingDelay + 10);
+        }
+
+        private async Task Work()
+        {
+            while (IsActive)
+            {
+                foreach (var joyState in _UsedInProfileJoystickStates)
+                {
+                    var diff = joyState.GetDifferents();
+                    foreach (var diffState in diff)
+                        await SendCommands(diffState.IsPressed
+                            ? diffState.PressKeyBindings
+                            : diffState.ReleaseKeyBindings);
+                }
+                await Task.Delay(PollingDelay);
+            }
+        }
+
+        private async Task SendCommands(List<KeyboardKeyBinding> keyBindings)
+        {
+            foreach (var binding in keyBindings)
+            {
+                if(binding.IsPress)
+                    _Sender.PressKey(binding.KeyCode);
+                else
+                    _Sender.ReleaseKey(binding.KeyCode);
+
+                await Task.Delay(InputDelay);
+            }
+
+        }
+    }
+}
