b930eded54856091d65fdea115bb4c249e758157
Денис <dultsevdv@gmail.com>
2022-08-04 04:58:57 +0500

    Обработчик расширенных действий кнопок

---
+++ JoyMapper/Services/ActionWatchers/ExtendedButtonActionWatcher.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using JoyMapper.Models;
+using JoyMapper.Models.JoyActions;
+
+namespace JoyMapper.Services.ActionWatchers
+{
+    internal class ExtendedButtonActionWatcher : ActionWatcherBase
+    {
+        private readonly ExtendedButtonJoyAction _ButtonJoyAction;
+
+        private readonly int _DoublePressDelay = 500;
+        private readonly int _LongPressDelay = 400;
+
+        private Stopwatch _DelayMeter; // Таймер задержки между нажатиями
+
+        private bool _FirstPressHandled; // Первое нажатие поймано
+
+        public ExtendedButtonActionWatcher(ExtendedButtonJoyAction buttonJoyAction)
+        {
+            _ButtonJoyAction = buttonJoyAction;
+        }
+
+        public override void Poll(JoyState joyState)
+        {
+            var btnState = _ButtonJoyAction.Button.Type switch
+            {
+                ButtonType.Button => joyState.Buttons[_ButtonJoyAction.Button.Value],
+                ButtonType.Pow1 => joyState.Pow1Value == _ButtonJoyAction.Button.Value,
+                ButtonType.Pow2 => joyState.Pow2Value == _ButtonJoyAction.Button.Value,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            var prewState = IsActive;
+            IsActive = btnState;
+
+            // Кнопка не нажата и прошло время двойного клика
+            if (_FirstPressHandled && !btnState && _DelayMeter?.ElapsedMilliseconds > _DoublePressDelay)
+            {
+                Debug.WriteLine("SinglePressSend: " + _ButtonJoyAction.Button);
+                SendSinglePress();
+                _DelayMeter = null;
+            }
+
+            // Кнопка нажата больше времени долгого нажатия
+            if (_FirstPressHandled && btnState && _DelayMeter?.ElapsedMilliseconds > _LongPressDelay)
+            {
+                Debug.WriteLine("LongPressSend: " + _ButtonJoyAction.Button);
+                SendLongPress();
+                _DelayMeter = null;
+            }
+
+
+            if (!prewState && btnState) // Состояние изменилось на нажатое
+            {
+                if (!_FirstPressHandled) // Регистрируем первое нажатие
+                {
+                    _FirstPressHandled = true;
+                    _DelayMeter = Stopwatch.StartNew();
+                }
+                else // Второе нажатие
+                {
+                    Debug.WriteLine("DoublePressSend: " + _ButtonJoyAction.Button);
+                    SendDoublePress();
+                    _FirstPressHandled = false;
+                    _DelayMeter = null;
+                }
+            }
+
+
+            IsActive = btnState;
+        }
+
+
+        private void SendSinglePress() => SendKeyboardCommands(_ButtonJoyAction.SinglePressKeyBindings);
+        private void SendDoublePress() => SendKeyboardCommands(_ButtonJoyAction.DoublePressKeyBindings);
+        private void SendLongPress() => SendKeyboardCommands(_ButtonJoyAction.LongPressKeyBindings);
+    }
+}
