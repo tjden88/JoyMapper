43681ca63974e56873a101e6bb7b2d47210c6160
Денис (home) <dultsevdv@gmail.com>
2022-12-17 01:55:08 +0500

    Реализация интерфейсов

--- JoyMapper/Services/JoyBindingsWatcher.cs
+++ JoyMapper/Services/JoyBindingsWatcher.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using JoyMapper.Models.JoyBindings.Base;
 using JoyMapper.Services.Interfaces;
 
@@ -6,18 +7,70 @@ namespace JoyMapper.Services;
 
 public class JoyBindingsWatcher : IJoyBindingsWatcher
 {
+    private readonly IJoystickStateManager _JoystickStateManager;
+
+    private List<JoyBindingsGroup> _BindingsGroups; // Привязки кнопок по имени джойстика
+
+    public JoyBindingsWatcher(IJoystickStateManager JoystickStateManager)
+    {
+        _JoystickStateManager = JoystickStateManager;
+    }
+
+
     public void StartWatching(IEnumerable<JoyBindingBase> bindings)
     {
-        throw new System.NotImplementedException();
+        var joyGroups = bindings
+            .GroupBy(b => b.JoyName)
+            .Select(g => new JoyBindingsGroup(g.Key, g.Select(gg=> (false, gg)).ToList() ))
+            .ToList();
+        _BindingsGroups = joyGroups;
+
+        SyncState();
     }
 
     public IEnumerable<JoyBindingBase> GetChanges()
     {
-        throw new System.NotImplementedException();
+        var changes = new List<JoyBindingBase>();
+        foreach (var joyBindingsGroup in _BindingsGroups)
+        {
+            var state = _JoystickStateManager.GetJoyState(joyBindingsGroup.JoyName);
+            if (state != null)
+            {
+                joyBindingsGroup.BindingStates.ForEach(gr =>
+                {
+                    var newState = gr.Binding.IsActive(state);
+                    if (gr is {LastState: true} != newState)
+                    {
+                        gr.LastState = newState;
+                        changes.Add(gr.Binding);
+                    }
+                });
+            }
+        }
+        return changes;
     }
 
     public void StopWatching()
     {
-        throw new System.NotImplementedException();
+        
     }
+
+
+    private void SyncState()
+    {
+        foreach (var joyBindingsGroup in _BindingsGroups)
+        {
+            var state = _JoystickStateManager.GetJoyState(joyBindingsGroup.JoyName);
+            if (state != null)
+            {
+                joyBindingsGroup.BindingStates.ForEach(gr =>
+                {
+                    gr.LastState = gr.Binding.IsActive(state);
+                });
+            }
+        }
+    }
+
+
+    private record JoyBindingsGroup(string JoyName, List<(bool LastState, JoyBindingBase Binding)> BindingStates);
 }
--- JoyMapper/Services/JoystickStateManager.cs
+++ JoyMapper/Services/JoystickStateManager.cs
@@ -1,6 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using JoyMapper.Helpers;
 using JoyMapper.Models;
 using JoyMapper.Services.Interfaces;
+using SharpDX.DirectInput;
 
 namespace JoyMapper.Services;
 
@@ -8,11 +13,34 @@ public class JoystickStateManager : IJoystickStateManager
 {
     public IEnumerable<string> GetConnectedJoysticks()
     {
-        throw new System.NotImplementedException();
+        var connectedDevices = new DirectInput()
+            .GetDevices(DeviceClass.GameControl, DeviceEnumerationFlags.AttachedOnly);
+
+        return connectedDevices.Select(cd => cd.InstanceName);
     }
+
 
     public JoyState GetJoyState(string joystickName)
     {
-        throw new System.NotImplementedException();
+        try
+        {
+            var deviceInstance = new DirectInput()
+                .GetDevices(DeviceClass.GameControl, DeviceEnumerationFlags.AttachedOnly)
+                .FirstOrDefault(d => d.InstanceName == joystickName);
+
+            if (deviceInstance == null) 
+                return null;
+
+            using var joystick = new Joystick(new DirectInput(), deviceInstance.InstanceGuid);
+            joystick.Acquire();
+            joystick.Poll();
+            return joystick.GetCurrentState().ToModel();
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine(e);
+            AppLog.LogMessage($"Ошибка опроса устройства - {joystickName}", LogMessage.MessageType.Error);
+            return null;
+        }
     }
 }
