cd2948b6d6909607cc70cd1c9c854b96b00616f2
Денис (home) <dultsevdv@gmail.com>
2022-12-16 22:59:01 +0500

    Интерфейс действия джойстика

---
+++ JoyMapper/Interfaces/IJoyBinding.cs
@@ -0,0 +1,29 @@
+﻿using JoyMapper.Models;
+
+namespace JoyMapper.Interfaces
+{
+    /// <summary>
+    /// Выбранная кнопка или ось джойстика
+    /// </summary>
+    internal interface IJoyBinding
+    {
+        /// <summary>
+        /// Имя привязанного джойстика
+        /// </summary>
+        string JoyName { get; }
+
+
+        /// <summary>
+        /// Активно ли действие в текущий момент
+        /// </summary>
+        /// <param name="joyState">Статус джойстика</param>
+        /// <returns></returns>
+        bool IsActive(JoyState joyState);
+
+
+        /// <summary>
+        /// Описание действия
+        /// </summary>
+        string Description { get; }
+    }
+}
