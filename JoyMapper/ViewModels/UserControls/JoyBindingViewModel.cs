a8b906df6a92f752b5c8a685ef9f84cd6b2069bf
Денис (home) <dultsevdv@gmail.com>
2022-12-17 21:41:33 +0500

    Заготовка вьюмодели биндинга джойстика

---
+++ JoyMapper/ViewModels/UserControls/JoyBindingViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using JoyMapper.Interfaces;
+using JoyMapper.Models.JoyBindings.Base;
+using WPR.MVVM.ViewModels;
+
+namespace JoyMapper.ViewModels.UserControls
+{
+    public class JoyBindingViewModel : ViewModel, IEditModel<JoyBindingBase>
+    {
+        public JoyBindingBase GetModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetModel(JoyBindingBase model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JoyMapper/Views/UserControls/JoyBindingView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace JoyMapper.Views.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для JoyBindingView.xaml
+    /// </summary>
+    public partial class JoyBindingView : UserControl
+    {
+        public JoyBindingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
