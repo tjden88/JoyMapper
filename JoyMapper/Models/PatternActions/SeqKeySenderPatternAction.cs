2f5acf1c25a1371622cfd6af656bfcfdb9e84bca
Денис (home) <dultsevdv@gmail.com>
2022-12-20 07:32:17 +0500

    Секвенции

---
+++ JoyMapper/Models/PatternActions/SeqKeySenderPatternAction.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using JoyMapper.Models.PatternActions.Base;
+using JoyMapper.Services;
+using JoyMapper.ViewModels.PatternActions;
+using JoyMapper.ViewModels.PatternActions.Base;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JoyMapper.Models.PatternActions;
+
+/// <summary>
+/// Секвенции (перебор комбинаций клавиш)
+/// </summary>
+public class SeqKeySenderPatternAction : PatternActionBase
+{
+
+    public List<List<KeyboardKeyBinding>> SeqKeysList { get; set; } = new();
+
+
+    public override PatternActionViewModelBase ToViewModel() => new SeqKeySenderViewModel(this);
+
+    
+    protected override void DoReportMode(bool newBindingState)
+    {
+        if(newBindingState)
+            ReportMessage?.Invoke("Активация действия");
+    }
+
+
+    #region Work
+
+    private int _CurrentIndex;
+    private KeyboardSender _KeyboardSender;
+
+    protected override void Initialize(IServiceProvider Services)
+    {
+        _KeyboardSender = Services.GetRequiredService<KeyboardSender>();
+        _CurrentIndex = 0;
+    }
+
+    protected override void DoWorkMode(bool newBindingState)
+    {
+        if(!newBindingState || SeqKeysList.Count == 0) return;
+
+        if (_CurrentIndex == SeqKeysList.Count)
+            _CurrentIndex = 0;
+
+        _KeyboardSender.SendKeyboardCommands(SeqKeysList[_CurrentIndex]);
+        _CurrentIndex++;
+    }
+} 
+#endregion
---
+++ JoyMapper/ViewModels/PatternActions/SeqKeySenderViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using JoyMapper.Models.PatternActions;
+using JoyMapper.Models.PatternActions.Base;
+using JoyMapper.ViewModels.PatternActions.Base;
+
+namespace JoyMapper.ViewModels.PatternActions;
+
+/// <summary>
+/// Секвенции (перебор комбинаций клавиш)
+/// </summary>
+public class SeqKeySenderViewModel : PatternActionViewModelBase
+{
+
+    public SeqKeySenderViewModel(SeqKeySenderPatternAction model)
+    {
+        if(model is null)
+            return;
+
+        SeqKeysList = new(model.SeqKeysList
+            .Select(key => 
+                new PatternActionKeysBindingViewModel("Последовательность команд")));
+    }
+
+
+    #region SeqKeysList : ObservableCollection<PatternActionKeysBindingViewModel> - Коллекция последовательностей
+
+    /// <summary>Коллекция последовательностей</summary>
+    private ObservableCollection<PatternActionKeysBindingViewModel> _SeqKeysList = new();
+
+    /// <summary>Коллекция последовательностей</summary>
+    public ObservableCollection<PatternActionKeysBindingViewModel> SeqKeysList
+    {
+        get => _SeqKeysList;
+        set => Set(ref _SeqKeysList, value);
+    }
+
+    #endregion
+
+
+    public override string Name => "Секвенция";
+    public override string Description => "Последовательная отправка команд клавиатуры при каждом активации действия";
+    public override PatternActionBase ToModel() =>
+        new SeqKeySenderPatternAction
+        {
+            SeqKeysList = SeqKeysList
+                .Select(kb => kb.KeyBindings.ToList())
+                .ToList(),
+        };
+
+    public override bool IsValid(out string ErrorMessage)
+    {
+        ErrorMessage = "Клавиатурные команды не назначены";
+        return SeqKeysList.Any() && SeqKeysList.All(kb => kb.KeyBindings.Any());
+    }
+}
