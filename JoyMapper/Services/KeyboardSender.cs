1ed0bcf3f38e6c9e9ad7798ca301548458d7f14d
Денис (home) <dultsevdv@gmail.com>
2022-12-20 20:07:06 +0500

    Добавление возможности эмуляци мыши

--- JoyMapper/Services/KeyboardSender.cs
+++ JoyMapper/Services/KeyboardSender.cs
@@ -2,8 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Security;
 using System.Windows.Input;
 
 namespace JoyMapper.Services;
@@ -16,17 +19,38 @@ public class KeyboardSender
     /// <summary> Эмулировать нажатие клавиши </summary>
     public void PressKey(Key key)
     {
-        KeyboardHook.Press(key);
+        Keyboard.Press(key);
         AppLog.LogKeyCommands($"Нажатие кнопки: {key}");
     }
 
     /// <summary> Эмулировать отпускание клавиши </summary>
     public void ReleaseKey(Key key)
     {
-        KeyboardHook.Release(key);
+        Keyboard.Release(key);
         AppLog.LogKeyCommands($"Отпускание кнопки: {key}");
     }
 
+    /// <summary> Эмулировать нажатие мыши </summary>
+    public void MousePress(MouseButton button)
+    {
+        Mouse.Down(button);
+        AppLog.LogKeyCommands($"Нажатие кнопки мыши: {button}");
+    }
+
+    /// <summary> Эмулировать отпускание мыши </summary>
+    public void MouseRelease(MouseButton button)
+    {
+        Mouse.Up(button);
+        AppLog.LogKeyCommands($"Отпускание кнопки мыши: {button}");
+    }
+
+    /// <summary> Эмулировать прокрутку мыши </summary>
+    public void MouseScroll(bool Up)
+    {
+        Mouse.Scroll(Up ? 3: -3);
+        var up = Up ? "Вверх" : "Вниз";
+        AppLog.LogKeyCommands($"Скролл мыши: {up}");
+    }
 
     /// <summary> Отправить клавиатурные команды в очередь команд </summary>
     public void SendKeyboardCommands(IEnumerable<KeyboardKeyBinding> keyboardKeyBindings)
@@ -66,6 +90,19 @@ public class KeyboardSender
         internal const int KeyeventfExtendedkey = 0x0001;
         internal const int KeyeventfKeyup = 0x0002;
         internal const int KeyeventfScancode = 0x0008;
+
+        internal const int MouseeventfVirtualdesk = 0x4000;
+
+        internal const int SMXvirtualscreen = 76;
+        internal const int SMYvirtualscreen = 77;
+        internal const int SMCxvirtualscreen = 78;
+        internal const int SMCyvirtualscreen = 79;
+
+        internal const int XButton1 = 0x0001;
+        internal const int XButton2 = 0x0002;
+        internal const int WheelDelta = 120;
+
+        internal const int InputMouse = 0;
         internal const int InputKeyboard = 1;
 
         // Various Win32 data structures
@@ -104,7 +141,23 @@ public class KeyboardSender
             internal int dwFlags;
             internal int time;
             internal IntPtr dwExtraInfo;
-        }
+        };
+
+        [Flags]
+        internal enum SendMouseInputFlags
+        {
+            Move = 0x0001,
+            LeftDown = 0x0002,
+            LeftUp = 0x0004,
+            RightDown = 0x0008,
+            RightUp = 0x0010,
+            MiddleDown = 0x0020,
+            MiddleUp = 0x0040,
+            XDown = 0x0080,
+            XUp = 0x0100,
+            Wheel = 0x0800,
+            Absolute = 0x8000,
+        };
 
         // Importing various Win32 APIs that we need for input
         [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
@@ -122,7 +175,235 @@ public class KeyboardSender
         #endregion
     }
 
-    internal static class KeyboardHook
+
+    /// <summary>
+    /// Exposes a simple interface to common mouse operations, allowing the user to simulate mouse input.
+    /// </summary>
+    /// <example>The following code moves to screen coordinate 100,100 and left clicks.
+    /// <code>
+    /**
+        Mouse.MoveTo(new Point(100, 100));
+        Mouse.Click(MouseButton.Left);
+    */
+    /// </code>
+    /// </example>
+    public static class Mouse
+    {
+        /// <summary>
+        /// Clicks a mouse button.
+        /// </summary>
+        /// <param name="mouseButton">The mouse button to click.</param>
+        public static void Click(MouseButton mouseButton)
+        {
+            Down(mouseButton);
+            Up(mouseButton);
+        }
+
+        /// <summary>
+        /// Double-clicks a mouse button.
+        /// </summary>
+        /// <param name="mouseButton">The mouse button to click.</param>
+        public static void DoubleClick(MouseButton mouseButton)
+        {
+            Click(mouseButton);
+            Click(mouseButton);
+        }
+
+        /// <summary>
+        /// Performs a mouse-down operation for a specified mouse button.
+        /// </summary>
+        /// <param name="mouseButton">The mouse button to use.</param>
+        public static void Down(MouseButton mouseButton)
+        {
+            switch (mouseButton)
+            {
+                case MouseButton.Left:
+                    SendMouseInput(0, 0, 0, NativeMethods.SendMouseInputFlags.LeftDown);
+                    break;
+                case MouseButton.Right:
+                    SendMouseInput(0, 0, 0, NativeMethods.SendMouseInputFlags.RightDown);
+                    break;
+                case MouseButton.Middle:
+                    SendMouseInput(0, 0, 0, NativeMethods.SendMouseInputFlags.MiddleDown);
+                    break;
+                case MouseButton.XButton1:
+                    SendMouseInput(0, 0, NativeMethods.XButton1, NativeMethods.SendMouseInputFlags.XDown);
+                    break;
+                case MouseButton.XButton2:
+                    SendMouseInput(0, 0, NativeMethods.XButton2, NativeMethods.SendMouseInputFlags.XDown);
+                    break;
+                default:
+                    throw new InvalidOperationException("Unsupported MouseButton input.");
+            }
+        }
+
+        /// <summary>
+        /// Moves the mouse pointer to the specified screen coordinates.
+        /// </summary>
+        /// <param name="point">The screen coordinates to move to.</param>
+        public static void MoveTo(Point point)
+        {
+            SendMouseInput(point.X, point.Y, 0, NativeMethods.SendMouseInputFlags.Move | NativeMethods.SendMouseInputFlags.Absolute);
+        }
+
+        /// <summary>
+        /// Resets the system mouse to a clean state.
+        /// </summary>
+        public static void Reset()
+        {
+            MoveTo(new Point(0, 0));
+
+            if (System.Windows.Input.Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                SendMouseInput(0, 0, 0, NativeMethods.SendMouseInputFlags.LeftUp);
+            }
+
+            if (System.Windows.Input.Mouse.MiddleButton == MouseButtonState.Pressed)
+            {
+                SendMouseInput(0, 0, 0, NativeMethods.SendMouseInputFlags.MiddleUp);
+            }
+
+            if (System.Windows.Input.Mouse.RightButton == MouseButtonState.Pressed)
+            {
+                SendMouseInput(0, 0, 0, NativeMethods.SendMouseInputFlags.RightUp);
+            }
+
+            if (System.Windows.Input.Mouse.XButton1 == MouseButtonState.Pressed)
+            {
+                SendMouseInput(0, 0, NativeMethods.XButton1, NativeMethods.SendMouseInputFlags.XUp);
+            }
+
+            if (System.Windows.Input.Mouse.XButton2 == MouseButtonState.Pressed)
+            {
+                SendMouseInput(0, 0, NativeMethods.XButton2, NativeMethods.SendMouseInputFlags.XUp);
+            }
+        }
+
+        /// <summary>
+        /// Simulates scrolling of the mouse wheel up or down.
+        /// </summary>
+        /// <param name="lines">The number of lines to scroll. Use positive numbers to scroll up and negative numbers to scroll down.</param>
+        public static void Scroll(double lines)
+        {
+            int amount = (int)(NativeMethods.WheelDelta * lines);
+
+            SendMouseInput(0, 0, amount, NativeMethods.SendMouseInputFlags.Wheel);
+        }
+
+        /// <summary>
+        /// Performs a mouse-up operation for a specified mouse button.
+        /// </summary>
+        /// <param name="mouseButton">The mouse button to use.</param>
+        public static void Up(MouseButton mouseButton)
+        {
+            switch (mouseButton)
+            {
+                case MouseButton.Left:
+                    SendMouseInput(0, 0, 0, NativeMethods.SendMouseInputFlags.LeftUp);
+                    break;
+                case MouseButton.Right:
+                    SendMouseInput(0, 0, 0, NativeMethods.SendMouseInputFlags.RightUp);
+                    break;
+                case MouseButton.Middle:
+                    SendMouseInput(0, 0, 0, NativeMethods.SendMouseInputFlags.MiddleUp);
+                    break;
+                case MouseButton.XButton1:
+                    SendMouseInput(0, 0, NativeMethods.XButton1, NativeMethods.SendMouseInputFlags.XUp);
+                    break;
+                case MouseButton.XButton2:
+                    SendMouseInput(0, 0, NativeMethods.XButton2, NativeMethods.SendMouseInputFlags.XUp);
+                    break;
+                default:
+                    throw new InvalidOperationException("Unsupported MouseButton input.");
+            }
+        }
+
+        /// <summary>
+        /// Sends mouse input.
+        /// </summary>
+        /// <param name="x">x coordinate</param>
+        /// <param name="y">y coordinate</param>
+        /// <param name="data">scroll wheel amount</param>
+        /// <param name="flags">SendMouseInputFlags flags</param>
+        [PermissionSet(SecurityAction.Assert, Name = "FullTrust")]
+        private static void SendMouseInput(int x, int y, int data, NativeMethods.SendMouseInputFlags flags)
+        {
+            PermissionSet permissions = new PermissionSet(PermissionState.Unrestricted);
+            permissions.Demand();
+
+            int intflags = (int)flags;
+
+            if ((intflags & (int)NativeMethods.SendMouseInputFlags.Absolute) != 0)
+            {
+                // Absolute position requires normalized coordinates.
+                NormalizeCoordinates(ref x, ref y);
+                intflags |= NativeMethods.MouseeventfVirtualdesk;
+            }
+
+            NativeMethods.INPUT mi = new NativeMethods.INPUT();
+            mi.type = NativeMethods.InputMouse;
+            mi.union.mouseInput.dx = x;
+            mi.union.mouseInput.dy = y;
+            mi.union.mouseInput.mouseData = data;
+            mi.union.mouseInput.dwFlags = intflags;
+            mi.union.mouseInput.time = 0;
+            mi.union.mouseInput.dwExtraInfo = new IntPtr(0);
+
+            if (NativeMethods.SendInput(1, ref mi, Marshal.SizeOf(mi)) == 0)
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+        }
+
+        private static void NormalizeCoordinates(ref int x, ref int y)
+        {
+            int vScreenWidth = NativeMethods.GetSystemMetrics(NativeMethods.SMCxvirtualscreen);
+            int vScreenHeight = NativeMethods.GetSystemMetrics(NativeMethods.SMCyvirtualscreen);
+            int vScreenLeft = NativeMethods.GetSystemMetrics(NativeMethods.SMXvirtualscreen);
+            int vScreenTop = NativeMethods.GetSystemMetrics(NativeMethods.SMYvirtualscreen);
+
+            // Absolute input requires that input is in 'normalized' coords - with the entire
+            // desktop being (0,0)...(65536,65536). Need to convert input x,y coords to this
+            // first.
+            //
+            // In this normalized world, any pixel on the screen corresponds to a block of values
+            // of normalized coords - eg. on a 1024x768 screen,
+            // y pixel 0 corresponds to range 0 to 85.333,
+            // y pixel 1 corresponds to range 85.333 to 170.666,
+            // y pixel 2 correpsonds to range 170.666 to 256 - and so on.
+            // Doing basic scaling math - (x-top)*65536/Width - gets us the start of the range.
+            // However, because int math is used, this can end up being rounded into the wrong
+            // pixel. For example, if we wanted pixel 1, we'd get 85.333, but that comes out as
+            // 85 as an int, which falls into pixel 0's range - and that's where the pointer goes.
+            // To avoid this, we add on half-a-"screen pixel"'s worth of normalized coords - to
+            // push us into the middle of any given pixel's range - that's the 65536/(Width*2)
+            // part of the formula. So now pixel 1 maps to 85+42 = 127 - which is comfortably
+            // in the middle of that pixel's block.
+            // The key ting here is that unlike points in coordinate geometry, pixels take up
+            // space, so are often better treated like rectangles - and if you want to target
+            // a particular pixel, target its rectangle's midpoint, not its edge.
+            x = ((x - vScreenLeft) * 65536) / vScreenWidth + 65536 / (vScreenWidth * 2);
+            y = ((y - vScreenTop) * 65536) / vScreenHeight + 65536 / (vScreenHeight * 2);
+        }
+    }
+
+    /// <summary>
+    /// Exposes a simple interface to common keyboard operations, allowing the user to simulate keyboard input.
+    /// </summary>
+    /// <example>
+    /// The following code types "Hello world" with the specified casing,
+    /// and then types "hello, capitalized world" which will be in all caps because
+    /// the left shift key is being held down.
+    /// <code>
+    /**
+            Keyboard.Type("Hello world");
+            Keyboard.Press(Key.LeftShift);
+            Keyboard.Type("hello, capitalized world");
+            Keyboard.Release(Key.LeftShift);
+    */
+    /// </code>
+    /// </example>
+    public static class Keyboard
     {
         #region Public Members
 
@@ -151,7 +432,7 @@ public class KeyboardSender
         {
             foreach (Key key in Enum.GetValues(typeof(Key)))
             {
-                if (key != Key.None && (Keyboard.GetKeyStates(key) & KeyStates.Down) > 0)
+                if (key != Key.None && (System.Windows.Input.Keyboard.GetKeyStates(key) & KeyStates.Down) > 0)
                 {
                     Release(key);
                 }
@@ -226,8 +507,12 @@ public class KeyboardSender
         /// </summary>
         /// <param name="key">Indicates the key pressed or released. Can be one of the constants defined in the Key enum.</param>
         /// <param name="press">True to inject a key press, false to inject a key release.</param>
+        [PermissionSet(SecurityAction.Assert, Name = "FullTrust")]
         private static void SendKeyboardInput(Key key, bool press)
         {
+            PermissionSet permissions = new PermissionSet(PermissionState.Unrestricted);
+            permissions.Demand();
+
             NativeMethods.INPUT ki = new NativeMethods.INPUT();
             ki.type = NativeMethods.InputKeyboard;
             ki.union.keyboardInput.wVk = (short)KeyInterop.VirtualKeyFromKey(key);
@@ -247,7 +532,7 @@ public class KeyboardSender
 
             ki.union.keyboardInput.dwFlags = dwFlags;
 
-            if (_ExtendedKeys.Contains(key))
+            if (ExtendedKeys.Contains(key))
             {
                 ki.union.keyboardInput.dwFlags |= NativeMethods.KeyeventfExtendedkey;
             }
@@ -271,25 +556,23 @@ public class KeyboardSender
         //
         // - docs appear to be incorrect. Use of Spy++ indicates that break is not an extended key.
         // Also, menu key and windows keys also appear to be extended.
-        private static readonly Key[] _ExtendedKeys = new Key[]
-        {
-            Key.RightAlt,
-            Key.RightCtrl,
-            Key.NumLock,
-            Key.Insert,
-            Key.Delete,
-            Key.Home,
-            Key.End,
-            Key.Prior,
-            Key.Next,
-            Key.Up,
-            Key.Down,
-            Key.Left,
-            Key.Right,
-            Key.Apps,
-            Key.RWin,
-            Key.LWin
-        };
+        private static readonly Key[] ExtendedKeys = new Key[] {
+                                                                   Key.RightAlt,
+                                                                   Key.RightCtrl,
+                                                                   Key.NumLock,
+                                                                   Key.Insert,
+                                                                   Key.Delete,
+                                                                   Key.Home,
+                                                                   Key.End,
+                                                                   Key.Prior,
+                                                                   Key.Next,
+                                                                   Key.Up,
+                                                                   Key.Down,
+                                                                   Key.Left,
+                                                                   Key.Right,
+                                                                   Key.Apps,
+                                                                   Key.RWin,
+                                                                   Key.LWin };
         // Note that there are no distinct values for the following keys:
         // numpad divide
         // numpad enter
