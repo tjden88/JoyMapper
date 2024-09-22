d7d33cc607ce1c5d759f224e8e1a730956eceee6
Денис <dultsevdv@gmail.com>
2024-09-22 16:40:20 +0500

    Модель девайса джойстика

---
+++ JoyMapper/Models/JoystickData.cs
@@ -0,0 +1,15 @@
+﻿namespace JoyMapper.Models
+{
+    /// <summary>
+    /// Данные устройства для определения привязок
+    /// </summary>
+    public record JoystickData
+    {
+        /// <summary>Идентификатор устройства</summary>
+        public string DeviceId { get; set; }
+
+
+        /// <summary> Отображаемое имя </summary>
+        public string DeviceName { get; set; }
+    }
+}
